7157cf95310b25dd2f909e63e27bf6462cc15a0f
Ryan Jones <rpjtx9@gmail.com>
2022-06-14 13:27:31 -0500

    Adding initial version of files

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+
+app = Flask(__name__)
+
+ 
+# Session will use filesystem
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
