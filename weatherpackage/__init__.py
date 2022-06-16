86fd9847cb34a06c407c4fc86b57addff65503ae
Ryan Jones <rpjtx9@gmail.com>
2022-06-16 14:25:48 -0500

    Created database via CLI and SQLAlchemy

--- weatherpackage/__init__.py
+++ weatherpackage/__init__.py
@@ -10,15 +10,19 @@ def create_app(test_config = None):
     app = Flask(__name__, instance_relative_config = True)
     app.config.from_mapping(
         SECRET_KEY = "dev",
-        DATABASE = os.path.join(app.instance_path, "weather.db")
+        SQLALCHEMY_DATABASE_URI = "sqlite:///"+os.path.join(app.instance_path, "weather.db"),
+        SQLALCHEMY_ECHO = True,
+        SQLALCHEMY_TRACK_MODIFICATIONS = False
     )
 
+    # Check for test configurations
     if test_config is None:
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
+
 
     # ensure the instance folder exists
     try:
@@ -30,4 +34,9 @@ def create_app(test_config = None):
     @app.route("/")
     def index():
         return render_template("index.html")
+
+    
+
+    from . import db
+    db.init_app(app)
     return app
--- weatherpackage/db.py
+++ weatherpackage/db.py
@@ -1,20 +1,45 @@
 from flask_sqlalchemy import SQLAlchemy
-
 import click
 from flask import current_app, g
 from flask.cli import with_appcontext
+import os
+
+# from sqlalchemy import String, Integer
 
 # Need to decide which one of these two database connection functions to use, if any! Might just use sqlitealchemy?
 # Function 1
 
 def get_db():
+    """Get the SQLAlchemy database object"""
     if 'db' not in g:
-        g.db = SQLAlchemy(app)
+        # Pass the application into SQLAlchemy object to create it
+        g.db = SQLAlchemy(current_app)
     return g.db
 
+def init_db():
+    """Command that will be executed via CLI due to click"""
+    db = get_db()
+    engine = db.create_engine("sqlite:///"+os.path.join(current_app.instance_path, "weather.db"),{})
+    meta = db.MetaData()
+    engine.execute("DROP TABLE IF EXISTS users;")
+    users = db.Table(
+        "users", meta,
+        db.Column("id", db.Integer, primary_key = True),
+        db.Column("username", db.String, unique = True, nullable = False),
+        db.Column("password_hashed", db.String, nullable = False),
+        db.Column("username_check", db.String, nullable = False)
+    )
+    meta.create_all(engine)
 
-def close_db(e=None):
-    db = g.pop('db', None)
 
-    if db is not None:
-        db.close()
+
+
+@click.command("init-db")
+@with_appcontext
+def init_db_command():
+    """Clear  existing data and create new tables"""
+    init_db()
+    click.echo("Initialized the database")
+
+def init_app(app):
+    app.cli.add_command(init_db_command)
