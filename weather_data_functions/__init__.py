a39d3cac0bfad974cfd5a228b5b351c6b1db6bf8
Ryan Jones <rpjtx9@gmail.com>
2022-06-23 13:33:41 -0500

    Removed info

--- weather_data_functions/__init__.py
+++ weather_data_functions/__init__.py
@@ -25,7 +25,7 @@ def weather_lookup(lat, lon, units="imperial"):
     """Takes the latitude and longitude of a location and returns weather data at that location. Defaults to imperial units, standard and metric are alternative options"""
     # Call the API
     try:
-        api_key = "211bb8b91bce61229a97d7fc04d2dcbb"       #os.environ.get("API_KEY") having a hard time with this at the moment
+        api_key = os.environ.get("API_KEY") 
         url = f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&units={units}&appid={api_key}"
         response = requests.get(url)
         response.raise_for_status()
